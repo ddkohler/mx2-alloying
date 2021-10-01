62b6dd45f7c4d68cca0514675fa426a793928aab
Daniel Kohler <11864045+ddkohler@users.noreply.github.com>
2021-10-01 11:33:05 -0500

    Update rc_vs_SiO2_thickness.py

--- figures/rc_vs_SiO2_thickness.py
+++ figures/rc_vs_SiO2_thickness.py
@@ -26,8 +26,9 @@ wt.artists.apply_rcparams(kind="publication")
 
 def run(save):
     # d2 = 75e-7
-    fig, gs = wt.artists.create_figure(width="dissertation", default_aspect=0.5)
-    ax = plt.subplot(gs[0])
+    fig, gs = wt.artists.create_figure(width="dissertation", cols=[1, 1])
+    ax0 = plt.subplot(gs[0])
+    ax1 = plt.subplot(gs[1])
     # plt.title(f"Reflection Contrast vs. SIO2 Thickness ")
     for i, d2i in enumerate(np.linspace(240, 340, 6)):
         # nmos2 = n_semi_1D(E0s_mos2, Gs_mos2, As_mos2, 0, offset_mos2)
@@ -41,22 +42,33 @@ def run(save):
         r_blank = blank.RT(hw)[0]
         r_sample = sample.RT(hw)[0]
         contrast = (r_sample - r_blank) / r_blank
-        ax.plot(hw, contrast, linewidth=3, alpha=0.5, label=label)
+        ax1.plot(hw, contrast, linewidth=3, alpha=0.5, label=label)
+        ax0.plot(hw, r_blank, linewidth=3, alpha=0.5, label=label)
 
     for y in [-15]:
         speci = d.chop("wm", at={"y":[y, "um"]})[0]
         # speci.print_tree()
-        ax.plot(
+        ax1.plot(
             speci.wm[:], speci.contrast[:],
             color="k", label="experiment \n" + r"($x=0 \ \mu \mathsf{m}, y=-16 \ \mu \mathsf{m}$)"
         )
-    l = ax.legend(fontsize=12, framealpha=0.7)
+    l = ax0.legend(fontsize=12, framealpha=0.7, bbox_to_anchor=(1.1, 1.2, 1.5, ), ncol=3)
+    wt.artists.corner_text("a", ax=ax0)
+    wt.artists.corner_text("b", ax=ax1)
     wt.artists.set_ax_labels(
-        ax,
+        ax1,
         xlabel=r"$\hbar \omega \ \left(\mathsf{eV} \right)$",
         ylabel=r"$(R - R_0) / R_0$"
     )
-    plt.grid()
+    ax1.yaxis.set_label_position("right")
+    ax1.yaxis.tick_right()
+    wt.artists.set_ax_labels(
+        ax0,
+        xlabel=r"$\hbar \omega \ \left(\mathsf{eV} \right)$",
+        ylabel=r"$R_0$"
+    )
+    ax0.grid()
+    ax1.grid()
     if save:
         wt.artists.savefig(here / "reflection_constrast_vs_SiO2_thickness.png")
     else:
@@ -64,4 +76,9 @@ def run(save):
 
 
 if __name__ == "__main__":
-    run(True)
+    from sys import argv
+    if len(argv) > 1:
+        save = argv[1] != "0"
+    else:
+        save = True
+    run(save)
